2f21fa5852ec24affb653b47b924488f0c996d98
wenha0 <921790476@qq.com>
2019-01-14 21:54:03 +0800

    更新pom文件 模块名称

---
+++ zookeeper-member-1/src/main/java/com/wenhao/cloudstudy/zookeepermember1/ZookeeperMember1Application.java
@@ -0,0 +1,14 @@
+package com.wenhao.cloudstudy.zookeepermember1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperMember1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperMember1Application.class, args);
+    }
+
+}
+
