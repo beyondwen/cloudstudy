0cdd92ae9e94f7e823a54aa3fb0dd48f595e814e
wenha0 <921790476@qq.com>
2019-01-16 23:50:04 +0800

    更新pom文件 模块名称 以及添加 zookeeper 为注册中心得模块

--- zookeeper-member-1/src/main/java/com/wenhao/cloudstudy/zookeepermember1/ZookeeperMember1Application.java
+++ zookeeper-member-1/src/main/java/com/wenhao/cloudstudy/zookeepermember1/ZookeeperMember1Application.java
@@ -2,8 +2,10 @@ package com.wenhao.cloudstudy.zookeepermember1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ZookeeperMember1Application {
 
     public static void main(String[] args) {
